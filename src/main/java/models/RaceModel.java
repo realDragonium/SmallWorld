d7b50f790a25ebfda3010574b8be23f09a30118d
keesdonder <keesdonder@gmail.com>
2019-06-06 10:41:48 +0200

    Mijn wijzigen

---
+++ src/main/java/models/RaceModel.java
@@ -0,0 +1,10 @@
+package main.java.models;
+
+public class RaceModel {
+    private krachtAction krachtAction;
+    private raceAction raceAction;
+    private boolean usable;
+    private boolean specialPower;
+    private int numberFiches;
+    private enum raceName {Dwarves ,Humans,Wizards,RatMen};
+}
