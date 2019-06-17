cf04493d812d51f593b182f5940bca1a16b6a527
realDragonium <beau@tormsed.nl>
2019-06-17 13:02:11 +0200

    Update FireBaseController.java

--- src/main/java/Controller/FireBaseController.java
+++ src/main/java/Controller/FireBaseController.java
@@ -1,112 +1,46 @@
 package Controller;
 
-import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import sun.rmi.runtime.Log;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
-public class FireBaseController {
+public class FirebaseController {
 
-    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/FireBaseKey.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://small-world-462b7.firebaseio.com")
-                .build();
-
-        FirebaseApp.initializeApp(options);
-
-        Firestore db = FirestoreClient.getFirestore();
-
-        FireBaseController app = new FireBaseController();
-
-        app.voegAccountToe(db, "dev", "admin");
-        app.update(db);
-        System.out.println(app.controlleerPassWord(db, "dev", "admin"));
-
-        // insert/ update
-//        app.update(db);
-//		// get
-//        app.getQuoteFromFirestore(db);
-//		// delete
-//		app.deleteFromFirestore(db);
+    public FirebaseController(){
+        loadFireBase();
+        getInformation();
     }
 
-    public void deleteFromFirestore(Firestore db) throws InterruptedException, ExecutionException {
-        DocumentReference docRef = db.collection("sampleData").document("my_new_document");
-        ApiFuture<WriteResult> future = docRef.delete();
-        WriteResult result = future.get();
+    public void loadFireBase() {
+        try {
+            System.out.println("Init van FireBase");
+            FileInputStream serviceAccount = new FileInputStream("/FireBaseKey.json");
 
-        System.out.println("Successfully deleted at: " + future.get().getUpdateTime());
-    }
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://small-world-462b7.firebaseio.com")
+                    .build();
 
+            FirebaseApp.initializeApp(options);
+        }
+        catch (FileNotFoundException e) {
 
-    private void getQuoteFromFirestore(Firestore db) throws InterruptedException, ExecutionException {
+        }
+        catch (IOException ie){
 
-        DocumentReference docRef = db.collection("sampleData").document("my_new_document");
-        ApiFuture<DocumentSnapshot> future = docRef.get();
-        DocumentSnapshot document = future.get();
-
-        if (document.exists()) {
-            String quote = (String) document.get("Gandhi");
-            System.out.println(quote);
-        } else {
-            System.out.println("No such document :(");
         }
     }
 
-    public void update(Firestore db) throws IOException, InterruptedException, ExecutionException {
-        // insert & update
-        // Als een document nog niet bestaat wordt het aangemaakt.
-        // Als een document al bestaat wordt het aagepast.
-        HashMap<String, String> quote = getSomethingToInsert();
+    public void getInformation(){
 
-        ApiFuture<WriteResult> future = db.collection("sampleData")
-                .document("my_new_document")
-                .set(quote);
-
-        System.out.println("Successfully updated at: "
-                + future.get().getUpdateTime());
-    }
-
-    private HashMap<String, String> getSomethingToInsert() throws IOException {
-        HashMap<String, String> quoteHashMap = new HashMap<String, String>();
-        quoteHashMap.put("Gandhi", "Hallo niemand");
-
-        return quoteHashMap;
-    }
-
-    public boolean controlleerPassWord(Firestore db, String username, String password) throws IOException, InterruptedException, ExecutionException {
-        DocumentReference docRef = db.collection("Accounts").document("admin");
-        ApiFuture<DocumentSnapshot> future = docRef.get();
-        DocumentSnapshot document = future.get();
-
-        if (document.exists()) {
-            String pass = (String) document.get("password");
-            return password.equals(pass);
-        } else {
-            System.out.println("No such document :(");
-        }
-        return false;
-    }
-
-    public void voegAccountToe(Firestore db, String username, String password) throws IOException, InterruptedException, ExecutionException {
-        Map<String, String> pass = new HashMap<>();
-        pass.put("password", password);
-        ApiFuture<WriteResult> future = db.collection("Accounts").document(username).set(pass);
-
-        System.out.println("Update time : " + future.get().getUpdateTime());
     }
 }
+
