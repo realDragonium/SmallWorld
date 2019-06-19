6da0dfb6b1d31c0cee4f1cde08b2ebe0c68c15c4
MartijnSteen <oefenhsleiden@gmail.com>
2019-06-19 12:16:57 +0200

    @overriding helemaal naar de getver

---
+++ src/main/tests/Controller/VervallenControllerTest.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VervallenControllerTest {
+
+    @Test
+    public void inVerval() {
+    }
+
+
+}
---
+++ src/main/tests/View/VervallenViewTest.java
@@ -0,0 +1,16 @@
+package View;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VervallenViewTest {
+
+    @Test
+    public void initialize() {
+    }
+
+    @Test
+    public void inVerval() {
+    }
+}
