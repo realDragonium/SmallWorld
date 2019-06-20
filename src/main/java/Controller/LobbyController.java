10c090808bbc4a180ee2434d1ed734cabef5e95d
LarsPB <50552064+LarsPB@users.noreply.github.com>
2019-06-20 11:25:26 +0200

    Java doc to lobbies

--- src/main/java/Controller/LobbyController.java
+++ src/main/java/Controller/LobbyController.java
@@ -1,47 +1,49 @@
 package Controller;
 
-
 import Applicatie.Applicatie;
 import Managers.SceneManager;
 import Model.LobbyModel;
 import Observer.LobbyObserver;
-
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class LobbyController {
 
+/**  This Controller-class is part of the MVC design pattern and shows the lobby screen (available lobbies).
+ * @author: Lars Puente Blom
+ * @version: June 2019
+ *
+ */
+
+public class LobbyController {
 	LobbyModel lobbymodel = new LobbyModel();
+
+
 
 	public LobbyController(){
 		SceneManager.getInstance().createLobbyView(this);
 	}
 
+
 	public void joinLobby(String Name){
 		Applicatie app = SceneManager.getInstance().getApp();
 		int id = app.getFirebaseService().joinLobby(Name, app.getAccountCon().getAccountName());
 		if(id>0){
-
 			new InLobbyController(Name, id);
 		}
-	}
-
-	public String getLobbyNaam(){
-		return lobbymodel.getLobbyNaam();
 	}
 
 	public void register(LobbyObserver ob) {
 		lobbymodel.register(ob);
 	}
 
-	public void hostLobby(String lobbyName) {
-		lobbymodel.hostLobby(lobbyName);
-	}
 
+	// Creates a new LobbySettingsController which gets its corresponding view in the SceneManager, by giving itself along
 	public void lobbyEdit(){
 		new LobbySettingsController();
 	}
 
+
+	// Tries to get a list of available lobbies.
 	public List<String> getFirebaseLobbyNamen(){
 		try {
 			return SceneManager.getInstance().getApp().getFirebaseService().getActiveLobbies();
@@ -54,27 +56,36 @@ public class LobbyController {
 	}
 
 
-	public void exitLobby(int decreaseLobbySize) {   
-		lobbymodel.exitLobby(decreaseLobbySize);
-	}
 
-	public void getLobbyAmount() {
-		lobbymodel.getLobbyAmount();
-	}
-
-
-	public void spelerToevoegen() {            // berekening om speler toe te voegen
-		lobbymodel.spelerToevoegen();
-	}
-
-	public void joinGame() {		// Joinen van een game
-		lobbymodel.joinGame();
-	}
-
-	public int getLobbySizeCounter() {
-		return lobbymodel.getLobbySizeCounter();
-	}
-
+//	public String getLobbyNaam(){
+//		return lobbymodel.getLobbyNaam();
+//	}
+//
+//	public void hostLobby(String lobbyName) {
+//		lobbymodel.hostLobby(lobbyName);
+//	}
+//
+//	public void exitLobby(int decreaseLobbySize) {
+//		lobbymodel.exitLobby(decreaseLobbySize);
+//	}
+//
+//	public void getLobbyAmount() {
+//		lobbymodel.getLobbyAmount();
+//	}
+//
+//
+//	public void spelerToevoegen() {            // berekening om speler toe te voegen
+//		lobbymodel.spelerToevoegen();
+//	}
+//
+//	public void joinGame() {		// Joinen van een game
+//		lobbymodel.joinGame();
+//	}
+//
+//	public int getLobbySizeCounter() {
+//		return lobbymodel.getLobbySizeCounter();
+//	}
+//
 
 
 }
