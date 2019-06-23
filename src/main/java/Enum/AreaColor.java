512194cd9a4066b8d5e602e7f8b93a9ebfaf23f1
realDragonium <beau@tormsed.nl>
2019-06-23 14:48:37 +0200

    fixed farm color

--- src/main/java/Enum/AreaColor.java
+++ src/main/java/Enum/AreaColor.java
@@ -3,7 +3,7 @@ package Enum;
 import javafx.scene.paint.Color;
 
 public enum AreaColor {
-    forest(Color.DARKGREEN), water(Color.AQUA),hill(Color.LIGHTGREEN), mountain(Color.GRAY), swamp(Color.BROWN), farm(Color.YELLOW);
+    forest(Color.DARKGREEN), water(Color.AQUA),hill(Color.LIGHTGREEN), mountain(Color.GRAY), swamp(Color.BROWN), farm(Color.YELLOW.darker());
     private Color color;
     AreaColor(Color color){
         this.color = color;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import Applicatie.Applicatie;
-import Controller.GameController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
